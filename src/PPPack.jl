43cb5d146c540f19649d19cd201a9cdf8add708a
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2017-06-06 16:47:10 +0200

    Added wrappers for INTERV and BSPLVB

--- src/PPPack.jl
+++ src/PPPack.jl
@@ -1,5 +1,27 @@
 module PPPack
 
-# package code goes here
+include("../deps/deps.jl")
+
+function interv(xt::Vector{Float32},
+                lxt::Integer, x::Float32,
+                left::Integer,mflag::Integer)
+    left = Int32[left]
+    mflag = Int32[mflag]
+    ccall((:interv_, _jl_libpppack),
+          Void,
+          (Ref{Float32}, Ref{Int32}, Ref{Float32}, Ref{Int32}, Ref{Int32}),
+          xt, Int32(lxt), x, left, mflag)
+    left[1],mflag[1]
+end
+
+function bsplvb(t::Vector{Float32}, jhigh::Integer, index::Integer,
+                  x::Float32, left::Integer, biatx::AbstractVector{Float32})
+    ccall((:bsplvb_, _jl_libpppack),
+          Void,
+          (Ref{Float32}, Ref{Int32}, Ref{Int32}, Ref{Float32}, Ref{Int32}, Ref{Float32}),
+          t, Int32(jhigh), Int32(index), x, Int32(left), biatx)
+end
+
+export interv, bsplvb
 
 end # module
