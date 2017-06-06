dacfca1c974777df89ceddbb32bf6be9696d4b36
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2017-06-06 16:46:44 +0200

    Added build of Carl F. de Boor's Fortran library pppack

---
+++ deps/build.jl
@@ -0,0 +1,24 @@
+using BinDeps
+
+@BinDeps.setup
+
+libpppack = library_dependency("libpppack")
+
+prefix = joinpath(BinDeps.depsdir(libpppack), "usr")
+src_dir = joinpath(BinDeps.depsdir(libpppack), "src", "pppack")
+build_dir = joinpath(BinDeps.depsdir(libpppack), "builds", "pppack")
+
+provides(SimpleBuild,
+         (@build_steps begin
+          CreateDirectory(build_dir)
+          @build_steps begin
+          ChangeDirectory(build_dir)
+          @build_steps begin
+          `cmake -DCMAKE_INSTALL_PREFIX="$prefix" $src_dir`
+          `make`
+          `make install`
+          end
+          end
+          end),
+         libpppack)
+@BinDeps.install Dict([(:libpppack, :_jl_libpppack)])
