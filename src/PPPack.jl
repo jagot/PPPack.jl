2a32ae4447180b70c44d69c219d833eea9ddfd70
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2017-06-07 11:44:27 +0200

    Added bsplvd wrapper.

--- src/PPPack.jl
+++ src/PPPack.jl
@@ -22,9 +22,22 @@ function bsplvb(t::Vector{Float32}, jhigh::Integer, index::Integer,
           t, Int32(jhigh), Int32(index), x, Int32(left), biatx)
 end
 
+function bsplvd(t::Vector{Float32}, k::Integer, x::Float32,
+                left::Integer, a::Matrix{Float32},
+                dbiatx::Matrix{Float32},
+                nderiv::Integer)
+    ccall((:bsplvd_, _jl_libpppack),
+          Void,
+          (Ref{Float32}, Ref{Int32}, Ref{Float32},
+           Ref{Int32}, Ref{Float32},
+           Ref{Float32},
+           Ref{Int32}),
+          t, Int32(k), x, Int32(left), a, dbiatx, nderiv)
+end
+
 pppbindir = abspath(joinpath(dirname(_jl_libpppack), "../../builds/pppack/bin"))
 example(exe) = `$(joinpath(pppbindir, exe))`
 
-export interv, bsplvb
+export interv, bsplvb, bsplvd
 
 end # module
